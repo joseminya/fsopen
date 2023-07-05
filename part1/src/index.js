a016ab44b4e2c322a1894859c09e2cc7c2b8cf26
joseminya <jose.minya@gmail.com>
2023-07-05 19:08:26 +0200

    part1 ejemplo

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +16,44 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old      </p>
+    </div>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'  
+  const age = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + age} />      
+      <Hello name={name} age={age} />  
+      <Footer />  
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <App />
+  
+)
+//ReactDOM.render(<App />, document.getElementById('root')) 
