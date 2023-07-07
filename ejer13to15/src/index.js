9d29a04600b8ae8a8122936edfa761e7b4bf26f0
joseminya <jose.minya@gmail.com>
2023-07-07 12:38:19 +0200

    ejer1.4

--- ejer13to15/src/index.js
+++ ejer13to15/src/index.js
@@ -4,25 +4,72 @@ import './index.css';
 //import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
+const Header = (props) => {
+  return (
+    <>
+    <h1>
+      {props.course}
+    </h1>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]}  />
+    <Part part={props.parts[2]}  />
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+     <p>
+      {props.name} {props.exercises}
+    </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  let count = 0;
+  props.parts.forEach(element => {
+    count+= element.exercises
+  });
+  return (
+    <>
+    <p>Number of exercises {count}</p>
+    </>
+  )
+}
+
+
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
-      ...
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
