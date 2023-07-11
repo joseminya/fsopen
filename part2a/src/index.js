4adad867a46bab219fcef9c6389deff384a2c15f
joseminya <jose.minya@gmail.com>
2023-07-11 11:02:37 +0200

    Introducción 2a

---
+++ part2a/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import Note from './Note';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true,
+  },
+]
+
+
+//const Note = ({ note }) => {
+//  return <li>{note.content}</li>
+//}
+/*
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+
+        {notes.map(note => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+*/
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App notes={notes}/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
